8b20501eb037edc725f18b77bf0d35e806a55a19
Summerschool <summerschool@Summerschools-MacBook-Pro.local>
2019-08-22 09:01:16 +0300

    day1

--- features/step_definitions/numbers_steps.rb
+++ features/step_definitions/numbers_steps.rb
@@ -1,3 +1,15 @@
-Given(/^I have selected "(.*)" as base numbers$/) do |type|
+And(/^I have selected "(.*)" as base numbers$/) do |type|
     @pages.numbers_screen.select_base_type type
+end
+
+And(/^I have selected "(.*)" as the conversion type$/) do |type|
+    @pages.numbers_screen.conversion_type type
+end
+
+When(/^I enter "(.*)" on the keyboard$/) do |number|
+    @pages.keyboard.input_number number
+end
+  
+Then(/^I see "(.*)" in the conversion section$/) do |number|
+    expect(@pages.numbers_screen.conversion_section_text).to eql number
 end
